a80ba2a77e4aa10898de99ea6c9892cb4eaa2942
taimoorkhan1122 <khant8k83@gmail.com>
2022-10-07 17:50:57 +0500

    fixed movies.ts 2

--- models/Movie.ts
+++ models/Movie.ts
@@ -38,15 +38,13 @@ export class Movie {
     borshInstructionSchema = borsh.struct([
         borsh.u8("variant"),
         borsh.str("title"),
-        borsh.str("rating"),
+        borsh.u8("rating"),
         borsh.str("description"),
     ]);
 
     //method for serializing data and slicing excess buffer
     serialize(): Buffer {
         const buffer = Buffer.alloc(1000);
-        console.log({ ...this });
-
         this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer);
         return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer));
     }
